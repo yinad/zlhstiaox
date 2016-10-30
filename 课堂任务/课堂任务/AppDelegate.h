baaf82cc7b832c6106419a39812aec3f6ab3f472
Adam <Yin_Y@yinyuandeMacBook-Pro.local>
2016-10-30 20:59:48 +0800

    创建项目

---
+++ 课堂任务/课堂任务/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  课堂任务
+//
+//  Created by Adam on 2016/10/30.
+//  Copyright © 2016年 YinY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- 课堂任务1/课堂任务1/Libs/AY_Addition/NSArray+Addition.h
+++
@@ -1,9 +0,0 @@
-
-
-#import <Foundation/Foundation.h>
-
-@interface NSArray (Addition)
-
-+ (NSArray *)ay_dataArrayWithPlistName:(NSString *)pName className:(NSString *)cName;
-
-@end
